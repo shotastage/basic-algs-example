3c65ea3060e99080cc7035006af979010fbcc94a
shotastage <shota.shimazu@magicalsoft.app>
2024-10-16 00:06:02 +0900

    add new alg

---
+++ logarithm/findprime.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void generatePrimeNumbers()
+{
+
+}
---
+++ logarithm/logarg.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int x = 81;
+
+    return 0;
+}
